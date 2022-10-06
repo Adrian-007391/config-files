ef7abbf022cdcb6ac80be199a14856984296ca7c
Adrian Eduardo Treviño Peña <adrian@Adrians-Mac.local>
2022-10-06 14:42:20 -0500

    telescope changes nvim

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -9,29 +9,30 @@ require('telescope').setup{
   },
   pickers = {
     find_files = {
+      theme = "dropdown"
     },
     buffers= {
+      theme = "dropdown"
     }
   },
   extensions = {
     file_browser={
+      theme = "dropdown",
       mappings={
-         ["n"] = {
+        ["n"] = {
           -- remap to going to home directory
-          ["<C-n>"] = fb_actions.create,
+          ["n"] = fb_actions.create,
           ["-"] =fb_actions.goto_parent_dir
         }
       }          
     }
   }
-      
+
 }
 require("telescope").load_extension "file_browser"
 Nnoremap("<leader>ff" ,"<cmd>Telescope find_files<cr>" )
 Nnoremap("<leader>hh" ,"<cmd>Telescope colorscheme<cr> " )
 Nnoremap("<leader>bb" ,"<cmd>Telescope buffers<cr> " )
 Nnoremap("<leader>fb" , "<cmd>Telescope file_browser<cr> <Esc>")
-
-vim.api.nvim_create_autocmd("VimEnter",{command="Telescope find_files"})
 
 
