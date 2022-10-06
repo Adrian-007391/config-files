bbe7b9b87f488fe408c57c0d4f1d47c0b1c7ecb1
Adrian Eduardo Treviño Peña <adrian@Adrians-Mac.local>
2022-10-06 14:43:30 -0500

    packer organization

--- nvim/after/plugin/packer.lua
+++ nvim/after/plugin/packer.lua
@@ -5,23 +5,25 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-use 'm4xshen/autoclose.nvim'
-use 'neovim/nvim-lspconfig'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-use { "nvim-telescope/telescope-file-browser.nvim" }
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
-use{"sheerun/vim-polyglot"}
- use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'neovim/nvim-lspconfig' -- lsp
+  use { "catppuccin/nvim", as = "catppuccin" } -- theme 
+  use 'wbthomason/packer.nvim' -- package manager
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  } -- fuzzy finder telescope
+  use { "nvim-telescope/telescope-file-browser.nvim" }-- file browser extension for telescope
+  use 'kyazdani42/nvim-web-devicons' --icon pack 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use{"sheerun/vim-polyglot"}
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use {
+    "windwp/nvim-autopairs",
+  }
 end)
