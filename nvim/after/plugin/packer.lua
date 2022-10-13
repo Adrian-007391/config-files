1f6581abd7795108ddb3afdad6acb5e38433ebd9
Adrian Eduardo Treviño Peña <adrian@Adrians-Mac.local>
2022-10-13 13:55:39 -0500

    new themes

--- nvim/after/plugin/packer.lua
+++ nvim/after/plugin/packer.lua
@@ -6,8 +6,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig' -- lsp
-  --use { "catppuccin/nvim", as = "catppuccin" } -- catppucchin theme 
---  use 'overcache/NeoSolarized' --neosolarized theme
+  use { "catppuccin/nvim", as = "catppuccin" } -- catppucchin theme 
+ use 'overcache/NeoSolarized' --neosolarized theme
   use 'navarasu/onedark.nvim'
   use 'wbthomason/packer.nvim' -- package manager
   use {
