6e4ed409ed65b04d761fdb22774b1583f77d1daf
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-10-28 20:34:03 -0500

    c-n c-p movement for search and buffers

--- nvim/after/plugin/Telescope.lua
+++ nvim/after/plugin/Telescope.lua
@@ -3,18 +3,27 @@ require('telescope').setup{
     mappings = {
       i = {
         ["<C-h>"] = "which_key"
-      }
-    }
+      },
+	  n = {
+		  ["<C-n>"] = "move_selection_next",
+		  ["<C-p>"] = "move_selection_previous",
+	  }
+}
   },
   pickers = {
 	  find_files={
-		  initial_mode = "insert"
-	  }
+		  initial_mode = "insert",
+	  },
+	  buffers = {
+		  initial_mode = "normal",
+		  theme = "dropdown",
+		  mappings  = {
+		  }
+	  },
 	  
   },
   extensions = {
   }
 }
 vim.api.nvim_set_keymap("n","<leader>ff","<Cmd>Telescope find_files<CR>", {silent= false, noremap = false})
-
-
+vim.api.nvim_set_keymap("n","<leader>bb","<Cmd>Telescope buffers<CR>", {silent= false, noremap = false})
