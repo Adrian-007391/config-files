5e13153d71543b767be306de02009fcb7e8dbebd
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-10-29 12:31:49 -0500

    resize remaps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -3,9 +3,8 @@ mf.nnoremap('<leader>fb','<Cmd>Ex<CR>')
 mf.nnoremap('<leader><CR>','<Cmd>wqa<CR>')
 mf.tnoremap('<C-w>','<C-\\><C-n>')
 
-mf.nnoremap('<C-h>','<Cmd>vertical resize -5<Cr>')
-mf.nnoremap('<C-l>','<Cmd>vertical resize +5<Cr>')
+mf.nnoremap('<C-.>','<Cmd>vertical resize -5<Cr>')
+mf.nnoremap('<C-,>','<Cmd>vertical resize +5<Cr>')
 
-mf.nnoremap('<C-k>','<Cmd>resize -5<Cr>')
-mf.nnoremap('<C-j>','<Cmd>resize +5<Cr>')
-
+mf.nnoremap('<C-[>','<Cmd>resize -5<Cr>')
+mf.nnoremap('<C-]>','<Cmd>resize +5<CR>')
