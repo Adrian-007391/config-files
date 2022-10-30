5005b0e6c2a24786358dcb2464a9715ccfe2dca1
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-10-30 14:01:17 -0600

    resize maps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,10 +1,13 @@
-mf = require("mapfunctions");
+mf=require("mapfunctions");
+-- file browser (netrw)
 mf.nnoremap('<leader>fb','<Cmd>Ex<CR>')
+-- close nvim
 mf.nnoremap('<leader><CR>','<Cmd>wqa<CR>')
+-- open terminal normal mode
 mf.tnoremap('<C-w>','<C-\\><C-n>')
 
-mf.nnoremap('<C-.>','<Cmd>vertical resize -5<Cr>')
-mf.nnoremap('<C-,>','<Cmd>vertical resize +5<Cr>')
+--resize windows
+mf.nnoremap('<leader>a','<Cmd>vertical resize +5<CR>')
+mf.nnoremap('<leader>s','<Cmd>vertical resize -5<CR>')
+mf.nnoremap('<leader>A','<Cmd>resize +5<CR>')
 
-mf.nnoremap('<C-[>','<Cmd>resize -5<Cr>')
-mf.nnoremap('<C-]>','<Cmd>resize +5<CR>')
