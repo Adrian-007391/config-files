2301949026c69fb2c6c4eefc475ecc1aa92a5b49
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-11-09 09:55:09 -0600

    terrsitter

---
+++ nvim/after/plugin/Treesitter.lua
@@ -0,0 +1,19 @@
+local status_ok , configs = pcall(require , "nvim-treesitter.configs")
+if not status_ok then 
+	return 
+end 
+
+configs.setup{
+	ensure_installed = {'cpp' , 'typescript' , 'lua'},
+	sync_install = false,
+	ignore_install = {""},
+	highlight = {
+		enable = true ,
+		disable = {'txt'},
+		additional_vim_regex_highligting = true,
+	},
+	indent = {
+		enable = true, 
+		disable = {""}
+	}
+}
