ddbf38561d530c2fe13edb9de69ed5890aff0b94
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-11-09 09:55:23 -0600

    astro command

---
+++ fish/functions/Astro.fish
@@ -0,0 +1,3 @@
+function Astro --wraps='cd ~/Documents/Coding🖥/Javascript_typescript/Astro/' --description 'alias Astro=cd ~/Documents/Coding🖥/Javascript_typescript/Astro/'
+  cd ~/Documents/Coding🖥/Javascript_typescript/Astro/ $argv; 
+end
