149e07aa96d784ad259b46eafc004231fe767b8e
Adrian Eduardo Treviño Peña <adrianeduardotrevinopena@Adrians-MacBook-Pro.local>
2022-12-17 16:15:06 -0600

    lsp diagnostics

--- nvim/after/plugin/Lsp.lua
+++ nvim/after/plugin/Lsp.lua
@@ -1,5 +1,16 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-lsp.setup()
 
+lsp.on_attach(function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_command [[augroup Format]]
+        vim.api.nvim_command [[autocmd! * <buffer>]]
+        vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+        vim.api.nvim_command [[augroup END]]
+    end
+end)
+
+
+
+lsp.setup()
