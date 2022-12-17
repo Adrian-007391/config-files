11303c7d229ac302b4f8a3db6864b9c0058e59b6
Adrian Eduardo Treviño Peña <adrianeduardotrevinopena@Adrians-MacBook-Pro.local>
2022-12-17 16:15:36 -0600

    Rainbow + autotag

--- nvim/lua/Adrian/packer.lua
+++ nvim/lua/Adrian/packer.lua
@@ -1,46 +1,51 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd("packadd packer.nvim")
 
-return _ENV["hello world"]('packer').startup(function(use)
-	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-    use{'ThePrimeagen/harpoon', requires = {'nvim-lua/plenary.nvim'}}
-    use "EdenEast/nightfox.nvim" 
-   use {
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use { 'ThePrimeagen/harpoon', requires = { 'nvim-lua/plenary.nvim' } }
+    use "EdenEast/nightfox.nvim"
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
-            local ts_update = _ENV["hello world"]('nvim-treesitter.install').update({ with_sync = true })
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end,
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
     }
 
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lua'},
-
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},
-    {'rafamadriz/friendly-snippets'},
-  }
-}
+    use('WhoIsSethDaniel/toggle-lsp-diagnostics.nvim')
+    use({
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    })
+    use("windwp/nvim-ts-autotag")
+    use("p00f/nvim-ts-rainbow")
 
 
 end)
-
