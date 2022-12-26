21a3163e60e6a47e3e495ca91213d9f9a14804aa
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2022-12-26 17:32:56 -0600

    colorscheeme search

--- nvim/after/plugin/Telescope.lua
+++ nvim/after/plugin/Telescope.lua
@@ -1,9 +1,9 @@
-local ok , builtin = pcall(require,"telescope.builtin")
-if not ok then 
-    return 
+local ok, builtin = pcall(require, "telescope.builtin")
+if not ok then
+    return
 end
 
-vim.keymap.set('n','<leader>pf',builtin.find_files,{})
-vim.keymap.set('n','<leader>pg',builtin.live_grep,{})
-vim.keymap.set('n','<leader>ph',builtin.help_tags,{})
-
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>pc', builtin.colorscheme, {})
