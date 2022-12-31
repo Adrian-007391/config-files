d4c309701b255fae3a1d3be48587ac01afabd6b8
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2022-12-30 18:25:47 -0600

    change in themes

--- nvim/after/plugin/Telescope.lua
+++ nvim/after/plugin/Telescope.lua
@@ -2,6 +2,25 @@ local ok, builtin = pcall(require, "telescope.builtin")
 if not ok then
     return
 end
+local telescope = require "telescope"
+telescope.setup(
+    {
+        pickers = {
+            find_files = {
+                theme = "dropdown"
+            },
+            live_grep = {
+                theme = "dropdown"
+            },
+            help_tags = {
+                theme = "dropdown"
+            },
+            colorscheme = {
+                theme = "dropdown"
+            }
+        }
+    }
+)
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
