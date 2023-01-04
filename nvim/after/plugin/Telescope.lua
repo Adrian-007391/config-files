d4227a842d8adbe8d66ab77212246ccab9d3f419
Adrian Eduardo Treviño Peña <eduardotrevinop@yahoo.com.mx>
2023-01-03 21:35:41 -0600

    buffers

--- nvim/after/plugin/Telescope.lua
+++ nvim/after/plugin/Telescope.lua
@@ -17,8 +17,11 @@ telescope.setup(
             },
             colorscheme = {
                 theme = "dropdown"
+            },
+            buffers = {
+                theme = "dropdown"
             }
-        }
+        },
     }
 )
 
@@ -26,3 +29,4 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>pc', builtin.colorscheme, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
